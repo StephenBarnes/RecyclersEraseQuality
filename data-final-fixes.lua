e2e6d100695a5a7d653043bb9e812adcd31f4b1a
Stephen Barnes <stephen6174@gmail.com>
2025-01-15 06:26:40 +0200

    Switch to recycle recipe ingredients as pseudo-fluids; basically working

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,23 +1,6 @@
 local Util = require("util")
+local TESTING = true
 
--- Collect all possible items output by recycling recipes.
-local maxNumOutputs = 0
-local recycleOutputItems = {}
-for _, recipe in pairs(data.raw.recipe) do
-	if recipe.category == "recycling" then
-		for _, product in pairs(recipe.results) do
-			if product.type == "item" then
-				recycleOutputItems[product.name] = true
-			end
-		end
-		maxNumOutputs = math.max(maxNumOutputs, #recipe.results)
-	end
-end
---log(serpent.block(recycleOutputItems))
---log(serpent.block(maxNumOutputs))
-
--- For each recycle output, create a placeholder fluid.
-local newFluids = {}
 local itemSubtypes = {
 	"item",
 	"capsule",
@@ -49,29 +32,57 @@ local function getItem(name)
 		if item then return item end
 	end
 end
-for itemName, _ in pairs(recycleOutputItems) do
-	local fluidName = "recycled-" .. itemName
-	local item = getItem(itemName)
-	if item == nil then error("Item " .. itemName .. " not found") end
-	---@type data.FluidPrototype
-	local fluid = {
-		type = "fluid",
-		name = fluidName,
-		localised_name = item.localised_name or {"item-name." .. itemName},
-		order = item.order,
-		subgroup = "fluid",
-		base_color = {r = 0.5, g = 0.5, b = 0.5},
-		flow_color = {r = 0.5, g = 0.5, b = 0.5},
-		default_temperature = 25,
-		hidden = true,
 
-		icon = item.icon,
-		icon_size = item.icon_size,
-		icons = item.icons,
-	}
-	table.insert(newFluids, fluid)
+-- For each recycle recipe, create a placeholder fluid for the input, and a recipe to solidify the fluid to the recycle recipe's outputs.
+for _, recipe in pairs(data.raw.recipe) do
+	if recipe.category == "recycling" and #recipe.ingredients == 1 then
+		local ingredientName = recipe.ingredients[1].name
+		local fluidName = "RECYCLING-" .. ingredientName
+		local item = getItem(ingredientName)
+		if item == nil then error("Item " .. ingredientName .. " not found") end
+
+		-- Create the new fluid.
+		---@type data.FluidPrototype
+		local fluid = {
+			type = "fluid",
+			name = fluidName,
+			localised_name = item.localised_name or {"item-name." .. ingredientName},
+			order = item.order,
+			subgroup = "fluid",
+			base_color = {r = 0.5, g = 0.5, b = 0.5},
+			flow_color = {r = 0.5, g = 0.5, b = 0.5},
+			default_temperature = 25,
+			hidden = not TESTING,
+
+			icon = recipe.icon,
+			icon_size = recipe.icon_size,
+			icons = recipe.icons,
+		}
+		data:extend{fluid}
+
+		-- Make the recycler's recipe output the fluid.
+		local originalResults = recipe.results
+		recipe.results = {{type = "fluid", name = fluidName, amount = 1}}
+
+		-- Make a new recipe to solidify the fluid.
+		local solidifyRecipe = {
+			type = "recipe",
+			name = "SOLIDIFY-" .. fluidName,
+			category = "solidify-recycled-items",
+			localised_name = {"recipe-name.solidify-recycling", item.localised_name or {"item-name." .. ingredientName}},
+			enabled = true,
+			hidden = not TESTING,
+			energy_required = 1,
+			ingredients = {{type = "fluid", name = fluidName, amount = 1}},
+			results = originalResults,
+
+			icon = recipe.icon,
+			icon_size = recipe.icon_size,
+			icons = recipe.icons,
+		}
+		data:extend{solidifyRecipe}
+	end
 end
-data:extend(newFluids)
 
 -- Create solidifier furnace entity that turns these fluids into items.
 ---@type data.FurnacePrototype
@@ -87,7 +98,7 @@ local solidifierEnt = Util.copyAndEdit(data.raw["assembling-machine"]["assemblin
 	source_inventory_size = 0,
 	energy_usage = "1W",
 	ingredient_count = 1,
-	crafting_speed = 100,
+	crafting_speed = 1000,
 	energy_source = {type = "void"},
 	subgroup = data.raw.item["recycler"].subgroup,
 	order = data.raw.item["recycler"].order .. "-1",
@@ -96,22 +107,24 @@ local solidifierEnt = Util.copyAndEdit(data.raw["assembling-machine"]["assemblin
 })
 data:extend{solidifierEnt}
 
--- Create item for solidifier.
-local solidifierItem = Util.copyAndEdit(data.raw.item["assembling-machine-2"], {
-	name = "recycle-solidifier",
-	place_result = "recycle-solidifier",
-	subgroup = data.raw.item["recycler"].subgroup,
-	order = data.raw.item["recycler"].order .. "-1",
-})
-data:extend{solidifierItem}
+if TESTING then
+	-- Create item for solidifier.
+	local solidifierItem = Util.copyAndEdit(data.raw.item["assembling-machine-2"], {
+		name = "recycle-solidifier",
+		place_result = "recycle-solidifier",
+		subgroup = data.raw.item["recycler"].subgroup,
+		order = data.raw.item["recycler"].order .. "-1",
+	})
+	data:extend{solidifierItem}
 
--- Create recipe for solidifier.
-local solidifierRecipe = Util.copyAndEdit(data.raw.recipe["assembling-machine-2"], {
-	name = "recycle-solidifier",
-	results = {{type = "item", name = "recycle-solidifier", amount = 1}},
-})
-data:extend{solidifierRecipe}
-table.insert(data.raw.technology["recycling"].effects, 2, {type = "unlock-recipe", recipe = "recycle-solidifier"})
+	-- Create recipe for solidifier.
+	local solidifierRecipe = Util.copyAndEdit(data.raw.recipe["assembling-machine-2"], {
+		name = "recycle-solidifier",
+		results = {{type = "item", name = "recycle-solidifier", amount = 1}},
+	})
+	data:extend{solidifierRecipe}
+	table.insert(data.raw.technology["recycling"].effects, 2, {type = "unlock-recipe", recipe = "recycle-solidifier"})
+end
 
 -- Create crafting category for solidifying recycled items.
 ---@type data.RecipeCategory
@@ -120,3 +133,19 @@ local solidifyRecycledItems = {
 	name = "solidify-recycled-items",
 }
 data:extend{solidifyRecycledItems}
+
+-- Give the recycling machine a fluid output.
+data.raw.furnace.recycler.fluid_boxes = {
+	{
+		production_type = "output",
+		pipe_picture = nil,
+		pipe_covers = nil,
+		base_area = 10,
+		base_level = -1,
+		pipe_connections = {{flow_direction = "output", position = {0.5, -1.6}, direction = defines.direction.north}},
+		volume = 1,
+	},
+}
+data.raw.furnace.recycler.result_inventory_size = 0
+
+-- TODO still need a manual recycling recipe
